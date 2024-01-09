4abb3a273c81d9cd395091508030635aa19177c0
przem97 <przemyslaw.przybysz97@gmail.com>
2024-01-09 13:06:22 +0100

    initial commit

---
+++ src/main/java/com/przem7/englishcourseapp/EnglishCourseStoreApplication.java
@@ -0,0 +1,13 @@
+package com.przem7.englishcourseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishCourseStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishCourseStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/przem7/englishcourseapp/EnglishCourseStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.przem7.englishcourseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishCourseStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
