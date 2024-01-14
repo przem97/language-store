c0f626679b70588dfe5810741eb6bda461562d01
przem97 <przemyslaw.przybysz97@gmail.com>
2024-01-14 22:51:13 +0100

    fix Word import

--- src/main/java/com/przem7/englishcourseapp/repository/WordRepository.java
+++ src/main/java/com/przem7/englishcourseapp/repository/WordRepository.java
@@ -1,6 +1,6 @@
 package com.przem7.englishcourseapp.repository;
 
-import com.przem7.englishcourseapp.model.Word;
+import com.przem7.englishcourseapp.model.orm.Word;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WordRepository extends CrudRepository<Word, Long> {
