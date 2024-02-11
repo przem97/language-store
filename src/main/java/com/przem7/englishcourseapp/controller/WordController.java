7fba1100ec96373c113a42da1666670c188131d6
przem97 <przemyslaw.przybysz97@gmail.com>
2024-02-11 18:03:33 +0100

    add REST endpoint to save answer

--- src/main/java/com/przem7/englishcourseapp/controller/WordController.java
+++ src/main/java/com/przem7/englishcourseapp/controller/WordController.java
@@ -1,12 +1,16 @@
 package com.przem7.englishcourseapp.controller;
 
+import com.przem7.englishcourseapp.exception.WordNotFoundException;
 import com.przem7.englishcourseapp.mapper.WordMapper;
+import com.przem7.englishcourseapp.model.dto.MatchDTO;
 import com.przem7.englishcourseapp.model.dto.WordDTO;
 import com.przem7.englishcourseapp.model.dto.WordStatisticsDTO;
 import com.przem7.englishcourseapp.model.orm.Word;
 
+import com.przem7.englishcourseapp.service.MatchService;
 import com.przem7.englishcourseapp.service.WordService;
 import com.przem7.englishcourseapp.service.WordStatisticsService;
+import com.przem7.englishcourseapp.validate.WordValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +26,12 @@ public class WordController {
 
     @Autowired
     private WordStatisticsService wordStatisticsService;
+
+    @Autowired
+    private MatchService matchService;
+
+    @Autowired
+    private WordValidator wordValidator;
 
     @Autowired
     private WordMapper wordMapper;
@@ -51,5 +61,14 @@ public class WordController {
     @GetMapping("/words/{wordId}/statistics")
     public ResponseEntity<WordStatisticsDTO> getStatisticsByWordId(@PathVariable("wordId") Long wordId) {
         return ResponseEntity.ok(wordStatisticsService.getWordStatisticsByWordId(wordId));
+    }
+
+    @PostMapping("/words/{wordId}/matches")
+    public ResponseEntity<Void> save(@PathVariable("wordId") Long wordId,
+                                     @RequestBody MatchDTO matchDto) throws WordNotFoundException {
+        wordValidator.validateIfExists(wordId);
+        matchService.save(wordId, matchDto);
+
+        return ResponseEntity.ok().build();
     }
 }
