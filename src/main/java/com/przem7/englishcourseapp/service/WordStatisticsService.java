9b64261a0bde24494345118e372c6bc256f2508e
przem97 <przemyslaw.przybysz97@gmail.com>
2024-02-11 15:27:33 +0100

    rename WordStatisticsDto to WordStatisticsDTO

--- src/main/java/com/przem7/englishcourseapp/service/WordStatisticsService.java
+++ src/main/java/com/przem7/englishcourseapp/service/WordStatisticsService.java
@@ -1,18 +1,10 @@
 package com.przem7.englishcourseapp.service;
 
-import com.przem7.englishcourseapp.model.dto.WordStatisticsDto;
-import com.przem7.englishcourseapp.model.orm.FailureMatch;
-import com.przem7.englishcourseapp.model.orm.SuccessMatch;
+import com.przem7.englishcourseapp.model.dto.WordStatisticsDTO;
 import com.przem7.englishcourseapp.model.orm.Word;
 import com.przem7.englishcourseapp.model.orm.WordStatistics;
-import com.przem7.englishcourseapp.repository.FailureMatchRepository;
 import com.przem7.englishcourseapp.repository.WordRepository;
 import com.przem7.englishcourseapp.repository.WordStatisticsRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +27,12 @@ public class WordStatisticsService {
     private ModelMapper mapper;
 
     @Transactional
-    public WordStatisticsDto getWordStatisticsByWordId(Long wordId) {
+    public WordStatisticsDTO getWordStatisticsByWordId(Long wordId) {
         Optional<Word> word = wordRepository.findById(wordId);
 
         if (word.isPresent()) {
             WordStatistics wordStatistics = wordStatisticsRepository.findTopByWord(word.get());
-            return mapper.map(wordStatistics, WordStatisticsDto.class);
+            return mapper.map(wordStatistics, WordStatisticsDTO.class);
         }
 
         return null;
