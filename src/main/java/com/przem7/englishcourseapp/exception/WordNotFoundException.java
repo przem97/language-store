51b533c0a4cc139a5223952044367c19df298fcf
przem97 <przemyslaw.przybysz97@gmail.com>
2024-02-11 18:04:18 +0100

    add WordNotFoundException

---
+++ src/main/java/com/przem7/englishcourseapp/exception/WordNotFoundException.java
@@ -0,0 +1,8 @@
+package com.przem7.englishcourseapp.exception;
+
+public class WordNotFoundException extends Exception {
+
+    public WordNotFoundException(String message) {
+        super(message);
+    }
+}
