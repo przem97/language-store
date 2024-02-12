d409bb94cc05600178d205fc0153f2306c5b1851
przem97 <przemyslaw.przybysz97@gmail.com>
2024-02-12 23:26:42 +0100

    WordMapper's convertToDto utility method

--- src/main/java/com/przem7/englishcourseapp/mapper/WordMapper.java
+++ src/main/java/com/przem7/englishcourseapp/mapper/WordMapper.java
@@ -1,5 +1,6 @@
 package com.przem7.englishcourseapp.mapper;
 
+import com.przem7.englishcourseapp.model.dto.WordDTO;
 import com.przem7.englishcourseapp.model.dto.WordDTOWithTranslations;
 import com.przem7.englishcourseapp.model.orm.Word;
 import org.modelmapper.ModelMapper;
@@ -16,6 +17,10 @@ public class WordMapper {
 
     public WordDTOWithTranslations convertToDtoWithTranslations(Word word) {
         return modelMapper.map(word, WordDTOWithTranslations.class);
+    }
+
+    public WordDTO convertToDto(Word word) {
+        return modelMapper.map(word, WordDTO.class);
     }
 
     public Word convertToEntity(WordDTOWithTranslations wordDto) {
