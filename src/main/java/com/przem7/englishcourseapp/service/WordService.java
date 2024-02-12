40b0e8ebf7c7969dd58c2e4c18d46cb8ed16b0b6
przem97 <przemyslaw.przybysz97@gmail.com>
2024-02-12 12:46:00 +0100

    improve WordService save method

--- src/main/java/com/przem7/englishcourseapp/service/WordService.java
+++ src/main/java/com/przem7/englishcourseapp/service/WordService.java
@@ -2,15 +2,19 @@ package com.przem7.englishcourseapp.service;
 
 import com.przem7.englishcourseapp.model.orm.Word;
 import com.przem7.englishcourseapp.repository.WordRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
+@Slf4j
 public class WordService {
 
     @Autowired
@@ -28,8 +32,27 @@ public class WordService {
         return wordRepository.findById(id);
     }
 
+    @Transactional
     public Word save(Word word) {
-        return wordRepository.save(word);
+        Set<Word> translations = word.getTranslations();
+        word.setTranslations(null);
+
+        word = wordRepository.save(word);
+
+        for (Word translation : translations) {
+            Word dbTranslation = wordRepository.findTopByValue(translation.getValue());
+
+            if (dbTranslation != null) {
+                dbTranslation.getTranslations().add(word);
+                Word savedTranslation = wordRepository.save(translation);
+                translation.setId(savedTranslation.getId());
+            }
+        }
+
+        word.setTranslations(translations);
+        Word saved = wordRepository.save(word);
+
+        return saved;
     }
 
     public void deleteById(Long wordId) {
