2abf0302353ac47f1ef710bba52621fc30f72b82
przem97 <przemyslaw.przybysz97@gmail.com>
2024-03-24 21:57:01 +0100

    make InvalidPayloadException extend EnglishCourseStoreException

--- src/main/java/com/przem7/englishcourseapp/exception/InvalidPayloadException.java
+++ src/main/java/com/przem7/englishcourseapp/exception/InvalidPayloadException.java
@@ -1,6 +1,6 @@
 package com.przem7.englishcourseapp.exception;
 
-public class InvalidPayloadException extends Exception {
+public class InvalidPayloadException extends EnglishCourseStoreException {
 
     public InvalidPayloadException(String message) {
         super(message);
